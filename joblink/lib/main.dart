f467b091ee1ca59426f63208b296ce7a6a6cf356
Edson3-eng <edsonaugustoandre@gmail.com>
2025-02-06 23:24:57 +0200

    main actualizado

--- joblink/lib/main.dart
+++ joblink/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:joblink/screens/auth/login.dart';
 import 'screens/auth/register_screen.dart';
 import 'package:sqflite/sqflite_dev.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
@@ -23,6 +24,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => RegisterScreen(),
+        '/login': (context) => LoginScreen(),
+        
       },
     );
   }
